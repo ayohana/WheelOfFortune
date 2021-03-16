851681eb9a2ea2ca3e5e1d47eb5c78122cc4dccb
Hamida Shaik <v-hshaik@microsoft.com>
2021-03-16 12:21:17 -0700

    Added SpinWheel class and SpinWheel Test

---
+++ WheelOfFortuneTest/SpinWheelTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WheelOfFortune;
+using System;
+
+
+namespace WheelOfFortuneTest
+{
+    [TestClass]
+    public class SpinWheelTest
+    { 
+        [TestMethod]
+        public void SpinWheelForCorrectGuessedLetter_CheckIsNotNull_ReturnTrue()
+        {
+            //Arrange
+             SpinWheel wheel = new SpinWheel();
+
+            //Act
+            int value = wheel.SpinWheelForCorrectGuessedLetter();
+
+            //Assert
+            Assert.IsNotNull(value);
+        }
+   
+        [TestMethod]
+        public void SpinWheelForCorrectWord_CheckIsNotNull_ReturnTrue()
+        {
+            //Arrange
+            SpinWheel wheel = new SpinWheel();
+
+            //Act
+            int value = wheel.SpinWheelForCorrectWord();
+
+            //Assert
+            Assert.IsNotNull(value);
+        }
+    }
+}
